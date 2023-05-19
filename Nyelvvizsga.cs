44b708e4c8793b87b7d70aabbdc3068d0485d327
GhostLead <72067053+GhostLead@users.noreply.github.com>
2023-05-19 10:15:59 +0200

    nem tudam mindent :(

---
+++ Nyelvvizsga.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Nyelvvizsga
+{
+    internal class Nyelvvizsga
+    {
+
+        string nyelv;
+        int ev2009;
+        int ev2010;
+        int ev2011;
+        int ev2012;
+        int ev2013;
+        int ev2014;
+        int ev2015;
+        int ev2016;
+        int ev2017;
+        int ev2018;
+        int nepszeruseg;
+
+        public Nyelvvizsga(string CSVsor)
+        {
+            var mezok = CSVsor.Split(';');
+            nyelv = mezok[0];
+            ev2009 = int.Parse(mezok[1]);
+            ev2010 = int.Parse(mezok[2]);
+            ev2011 = int.Parse(mezok[3]);
+            ev2012 = int.Parse(mezok[4]);
+            ev2013 = int.Parse(mezok[5]);
+            ev2014 = int.Parse(mezok[6]);
+            ev2015 = int.Parse(mezok[7]);
+            ev2016 = int.Parse(mezok[8]);
+            ev2017 = int.Parse(mezok[9]);
+            ev2018 = int.Parse(mezok[10]);
+            nepszeruseg = ev2009 + ev2010 + ev2011 + ev2012 + ev2013 + ev2014 + ev2015 + ev2016 + ev2017 + ev2018;
+        }
+
+        public Nyelvvizsga(string nyelv, int ev2009, int ev2010, int ev2011, int ev2012, int ev2013, int ev2014, int ev2015, int ev2016, int ev2017, int ev2018, int nepszeruseg)
+        {
+            this.nyelv = nyelv;
+            this.ev2009 = ev2009;
+            this.ev2010 = ev2010;
+            this.ev2011 = ev2011;
+            this.ev2012 = ev2012;
+            this.ev2013 = ev2013;
+            this.ev2014 = ev2014;
+            this.ev2015 = ev2015;
+            this.ev2016 = ev2016;
+            this.ev2017 = ev2017;
+            this.ev2018 = ev2018;
+            this.nepszeruseg = nepszeruseg;
+        }
+
+        public string Nyelv { get => nyelv; set => nyelv = value; }
+        public int Ev2009 { get => ev2009; set => ev2009 = value; }
+        public int Ev2010 { get => ev2010; set => ev2010 = value; }
+        public int Ev2011 { get => ev2011; set => ev2011 = value; }
+        public int Ev2012 { get => ev2012; set => ev2012 = value; }
+        public int Ev2013 { get => ev2013; set => ev2013 = value; }
+        public int Ev2014 { get => ev2014; set => ev2014 = value; }
+        public int Ev2015 { get => ev2015; set => ev2015 = value; }
+        public int Ev2016 { get => ev2016; set => ev2016 = value; }
+        public int Ev2017 { get => ev2017; set => ev2017 = value; }
+        public int Ev2018 { get => ev2018; set => ev2018 = value; }
+        public int Nepszeruseg { get => nepszeruseg; set => nepszeruseg = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+//    Amir tudtam azt megcsináltam, viszont sajnos így is volt ami nem ment :(
+//    A hétvégén megpróbálom jobban megérteni és alkalmazni a LINQ-t.
+
+using LINQ_Nyelvvizsga;
+using System.Text.Unicode;
+
+List<Nyelvvizsga> nyelvvizsgak = new List<Nyelvvizsga>();
+
+StreamReader sr = new StreamReader("sikeres.csv",encoding:System.Text.Encoding.UTF8);
+
+sr.ReadLine(); // első sor "kihagyása"
+while (!sr.EndOfStream)
+{
+    nyelvvizsgak.Add(new Nyelvvizsga(sr.ReadLine()));
+}
+sr.Close();
+
+/*
+sr = new StreamReader("sikertelen.csv", encoding: System.Text.Encoding.UTF8);
+
+sr.ReadLine(); // első sor "kihagyása"
+while (!sr.EndOfStream)
+{
+    sikertelen_nyelvvizsgak.Add(new Nyelvvizsga(sr.ReadLine()));
+}
+sr.Close();
+*/
+
+// 2.feladat
+
+var top3 = nyelvvizsgak.OrderBy(x => x.Nepszeruseg).Take(3).ToList();
+foreach (var item in top3)
+{
+    Console.WriteLine(item.Nyelv);
+}
+
+// 3. feladat
+
+Console.Write("Adjon meg egy évet! : ");
+int input = int.Parse(Console.ReadLine());
+
+while (input > 2018 || input < 2009)
+{
+    Console.WriteLine("Hibás megadás!");
+    Console.Write("Adjon meg egy évet! : ");
+    input = int.Parse(Console.ReadLine());
+}
+string evInput = "ev" + input.ToString();
+
+// 4. feladat
+
+
+// 5. feladat
+// mivel az előzőt nem tudtam, így ezt nemt tudom megcsinálni
+
+// 6. feladat
+// mivel az előző kettőt nem tudtam, így ezt nem tudom megcsinálni
+
+
